fb3ac516484bcebf29a24a9c1bb0dc3d577e31a2
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-08 15:25:17 +0100

    declared variables and added a function in script.js

--- script/script.js
+++ script/script.js
@@ -1,30 +1,36 @@
 "use strict";
 console.clear();
 
-// const priceKm = 0.21;
-// const juniorDiscount = 20;
-// const seniorDiscount = 40;
-// const junior = 17;
-// const senior = 65;
+const priceKm = 0.21;
+const juniorDiscount = 20;
+const seniorDiscount = 40;
+const junior = 17;
+const senior = 65;
 
-// const totKm = document.getElementById('totKm');
+const totKm = parseInt(document.getElementById('totKm'));
+const passengerAge = parseInt(document.getElementById('age'));
+const passengerName = document.getElementById('nameLastname');
+const result = document.getElementById('finalPrice');
+const myTicket = document.getElementById('ticket');
 
-// const passengerAge = document.getElementById('age');
+const totPrice = totKm * priceKm;
+console.log(totPrice);
 
-// const totPrice = totKm * priceKm;
-// console.log(totPrice);
+let message;
+let finalPrice;
 
-// let message;
-// let finalPrice;
+myTicket.addEventListener("submit", function (event) {
+    if (passengerAge <= junior) {
+        finalPrice = totPrice - ((totPrice * juniorDiscount) / 100);
+        message = "E' stato applicato uno sconto del 20% riservato ai minorenni. Il costo finale del tuo biglietto è di €" + finalPrice.toFixed(2);
+    } else if (passengerAge >= senior) {
+        finalPrice = totPrice - ((totPrice * seniorDiscount) / 100);
+        message = "E' stato applicato uno sconto del 40% riservato agli over 65. Il costo finale del tuo biglietto è di €" + finalPrice.toFixed(2);
+    } else {
+        message = "Il costo del tuo biglietto è di €" + totPrice.toFixed(2);
+    }
+    event.preventDefault();
+    result.innerHTML = message;
+});
 
-// if (passengerAge <= junior) {
-//     finalPrice = totPrice - ((totPrice * juniorDiscount) / 100);
-//     message = "E' stato applicato uno sconto del 20% riservato ai minorenni. Il costo finale del tuo biglietto è di €" + finalPrice.toFixed(2);
-// } else if (passengerAge >= senior) {
-//     finalPrice = totPrice - ((totPrice * seniorDiscount) / 100); 
-//     message = "E' stato applicato uno sconto del 40% riservato agli over 65. Il costo finale del tuo biglietto è di €" + finalPrice.toFixed(2);
-// } else {
-//     message = "Il costo del tuo biglietto è di €" + totPrice.toFixed(2);
-// }
-
-// console.log(message);
+console.log(message);
