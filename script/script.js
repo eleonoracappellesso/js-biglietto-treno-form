78cdf8bcf51abd6151c16b21e2879084805cd7f4
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-08 15:05:27 +0100

    added form elements in main of index html

--- script/script.js
+++ script/script.js
@@ -1,2 +1,30 @@
 "use strict";
 console.clear();
+
+// const priceKm = 0.21;
+// const juniorDiscount = 20;
+// const seniorDiscount = 40;
+// const junior = 17;
+// const senior = 65;
+
+// const totKm = document.getElementById('totKm');
+
+// const passengerAge = document.getElementById('age');
+
+// const totPrice = totKm * priceKm;
+// console.log(totPrice);
+
+// let message;
+// let finalPrice;
+
+// if (passengerAge <= junior) {
+//     finalPrice = totPrice - ((totPrice * juniorDiscount) / 100);
+//     message = "E' stato applicato uno sconto del 20% riservato ai minorenni. Il costo finale del tuo biglietto è di €" + finalPrice.toFixed(2);
+// } else if (passengerAge >= senior) {
+//     finalPrice = totPrice - ((totPrice * seniorDiscount) / 100); 
+//     message = "E' stato applicato uno sconto del 40% riservato agli over 65. Il costo finale del tuo biglietto è di €" + finalPrice.toFixed(2);
+// } else {
+//     message = "Il costo del tuo biglietto è di €" + totPrice.toFixed(2);
+// }
+
+// console.log(message);
